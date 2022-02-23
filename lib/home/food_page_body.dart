a707b5048f1b7a85f1ebe1155f86c9b2b80010e8
Sahil Titarmare <titarmaresahil2003@gmail.com>
2022-02-23 12:02:28 -0800

    commit

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -87,8 +87,23 @@ class _FoodPageBodyState extends State<FoodPageBody>{
             height: 120,
             margin: const EdgeInsets.only(left: 30,right: 30,bottom: 30),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+              boxShadow:  [
+                BoxShadow(
+                  color: Color(0xFFe8e8e8),
+                  blurRadius: 5.0,
+                  offset: Offset(0 , 5)
+                ),
+                BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(-5,0)
+                ),
+                 BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(5,0)
+                )
+              ]
             ),
             child: Container(
               padding: EdgeInsets.only(top: 10,left: 15,right: 15),
